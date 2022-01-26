3e220a9ad6f119e026ce53b3d97318dc448c4ad0
osherz80 <88611368+osherz80@users.noreply.github.com>
2022-01-26 23:46:32 +0200

    Add files via upload

--- netlify.js
+++ netlify.js
@@ -7,32 +7,6 @@ const showDropMenu = () => {
 
 
 
-axios.get('http://data.nba.net/data/10s/prod/v1/2019/players.json')  
-.then((response) => {
-    let players = response.data.league.standard;
-    let randomArr = [30,450,35,75];
-    let cards = document.querySelectorAll(".card");
-    console.log(response);
-
-
-    let i = 0;
-    cards.forEach((card)=>{
-        card.querySelector(".cardName span").innerHTML = players[randomArr[i]].temporaryDisplayName;
-        card.querySelector(".circle button").style.backgroundImage = `url(https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${players[randomArr[i]].personId}.png`});
-        card.querySelector(".jersy").innerHTML = players[randomArr[i]].jersey;
-        card.querySelector(".pos").innerHTML = players[randomArr[i]].pos;
-
-        i++;
-        console.log(card);
-   
-   
-    
-       
-    })
-
- .catch((error)=>{
-     console.log(error);
- })    
   
 
 
